400356b64520c78bada23e8ca391f582ad986743
Sankhyahrick Swami <sankhyahricks@gmail.com>
2024-12-31 08:41:35 +0530

    3rd

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module Fiber
 go 1.23.4
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/skip2/go-qrcode"
 )
 
-func main(){
-	app:=fiber.New()
-	app.Static("/", "./Frontend")
-	fmt.Println("http://localhost:8000")
-	app.Listen(":8000")
+func main() {
+	app := fiber.New()
 
+	// Serve the main HTML page
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("./Frontend/index.html")
+	})
+
+	// QR code generation endpoint
+	app.Post("/generate", func(c *fiber.Ctx) error {
+		text := c.FormValue("text")
+
+		// Generate QR code
+		png, err := qrcode.Encode(text, qrcode.Medium, 256)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Failed to generate QR code")
+		}
+
+		// Send the generated QR code as an image
+		c.Set("Content-Type", "image/png")
+		return c.Send(png)
+	})
+
+	fmt.Println("Server is running at http://localhost:8000")
+	app.Listen(":8000")
 }
